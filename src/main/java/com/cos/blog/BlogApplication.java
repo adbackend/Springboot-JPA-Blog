fc98eebc09a17d38b138e7d2db7e0bb1a40ed517
yujeong <yyyy2549@gmail.com>
2022-03-29 09:27:36 +0900

    테스트제발

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -11,5 +11,11 @@ public class BlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+	
+	
+	public String aa() {
+		
+		return "깃테스트 제발~~~~ㅠㅠ";
+	}
 
 }
