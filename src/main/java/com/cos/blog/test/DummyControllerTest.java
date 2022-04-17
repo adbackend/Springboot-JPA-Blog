2bf93657866953886a1ffa07068d2d78a6308422
yujeong <yyyy2549@gmail.com>
2022-04-17 23:43:49 +0900

    @Transactional

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,11 +1,20 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -19,6 +28,52 @@ public class DummyControllerTest {
 	
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
+	
+//	public
+	
+	
+	
+	//save 함수는 id를 전달하지 않으면 insert를 해주고
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다
+	//email, password
+	
+	@Transactional //함수 종료시에 자동 commit이 된다
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json 데이터를 요청 -> Java Object(MessageConverter의 Jacson라이브러리가 변환해서 받음)
+		System.out.println("id: "+ id);
+		System.out.println("password: "+ requestUser.getPassword());
+		System.out.println("email: "+ requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+//		userRepository.save(user);
+		
+		//더티체킹
+		return user;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	//한 페이지당 2건에 데이터를 리턴받아 볼 예정
+	@GetMapping("dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable){
+		
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		
+		return users;
+	} 
 	
 	// {id} 주소로 파라미터를 전달 받을 수 있다
 	// http://localhost:8000/blog/dummy/user/3
